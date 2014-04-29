a5fd17d26a31b392641a452f3028a33c1d936ff1
Guilherme Farias <guilherme@guiky.com.br>
2014-04-28 21:35:50 -0300

    Adding Note class

---
+++ src/Note.java
@@ -0,0 +1,24 @@
+
+public class Note {
+	private String text;
+	private int userId;
+
+	public void setText(String text){
+		this.text = text;
+	}
+
+	public void setUserId(int userId){
+		this.userId = userId;
+	}
+
+	public String getText(){
+		return this.text;
+	}
+
+	public int getUserId(){
+		return this.userId;
+	}
+
+	public Note(){
+	}
+}
