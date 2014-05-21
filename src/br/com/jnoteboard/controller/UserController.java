9ad8e34d6635c86b63afee2d07b2544478b0e595
Guilherme Farias <guilherme@guiky.com.br>
2014-05-21 00:31:30 -0300

    Redirecting user from login page to home page whem the form is submitted

--- src/br/com/jnoteboard/controller/UserController.java
+++ src/br/com/jnoteboard/controller/UserController.java
@@ -1,25 +1,27 @@
 package br.com.jnoteboard.controller;
 
-import br.com.jnoteboard.entity.User;
+import java.io.IOException;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 public class UserController {
 	private String email;
 	private String password;
-	private User userLogged;
 	private boolean loggedIn;
 
 	public String getEmail() {
 		return email;
 	}
-   
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-   
+
 	public String getPassword() {
 	  	return password;
 	}
-   
+
 	public void setPassword(String password){
 		this.password = password;
 	}
@@ -28,12 +30,25 @@ public class UserController {
 	  	return this.loggedIn;
 	}
 
-	public String doLogin(){
-		return "";
+	public void doLogin() throws IOException{
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+
+		if (!this.email.isEmpty() && !this.password.isEmpty()){
+			this.loggedIn = true;
+			HttpSession httpSession = (HttpSession) externalContext.getSession(true);
+			httpSession.setAttribute("userController", this);
+			externalContext.redirect("/JNoteboard/faces/home.xhtml");
+		} else {
+			externalContext.redirect("/JNoteboard/faces/login.xhtml");
+		}
 	}
 
-	public String doLogout(){
-		return "";
+	public void doLogout() throws IOException{
+		this.loggedIn = false;
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		HttpSession httpSession = (HttpSession) externalContext.getSession(true);
+		httpSession.removeAttribute("userController");
+		externalContext.redirect("/JNoteboard/faces/login.xhtml");
 	}
 
 	public UserController(){
